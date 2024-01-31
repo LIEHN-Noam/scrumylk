2d655ebd3f4043d772ad74811f2c23483bb058d9
Yagba Kipré <yagkip5@gmail.com>
2024-01-31 19:08:29 +0000

    début de dashboard

---
+++ site/dashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="./css/dash.scss">
+</head>
+<body>
+<section class="app">
+  <aside class="sidebar">
+         <header>
+        Menu
+      </header>
+    <nav class="sidebar-nav">
+ 
+      <ul>
+        <li>
+          <a href="#"><i class="ion-bag"></i> <span>Shop</span></a>
+          
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-settings"></i> <span class="">Controls</span></a>
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-briefcase-outline"></i> <span class="">Folio</span></a>
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-analytics-outline"></i> <span class="">Graphicals</span></a>
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-paper-outline"></i> <span class="">Papers</span></a>
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-navigate-outline"></i> <span class="">Ass Finder</span></a>
+        </li>
+        <li>
+          <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Cocaine</span></a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</section>
+</body>
+</html>
